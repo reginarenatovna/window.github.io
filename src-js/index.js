e9ff6288a0cccd56cab4e86e3f95107190f0d665
Regina <regina-saifieva@mail.ru>
2018-11-01 20:18:44 +0300

    settineoutPopup

--- src-js/index.js
+++ src-js/index.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded',  function(){
 
 	'use strict';
-	let timer = require('./parts/timer.js');
+	let timer = require('./parts/timer.js'),
+			form = require('./parts/form.js');
 	timer();
+	form();
 });
