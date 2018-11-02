528ecd3d9081682fa4a7d6a64930a32c29ff3187
Regina <regina-saifieva@mail.ru>
2018-11-02 19:47:59 +0300

    change

--- src-js/parts/form.js
+++ src-js/parts/form.js
@@ -1,33 +1,26 @@
 function form() {
 	let popup = document.querySelector('.popup'),
-			close = document.querySelectorAll('.popup_close'),
 			headerBtn = document.querySelector('.header_btn'),
-			popupDialog = document.querySelectorAll('popup_dialog'),
+			popupForm = document.querySelectorAll('.popup_form'),
 			popupEngineer = document.querySelector('.popup_engineer');
 setTimeout(function(){
 popup.style.display = 'block';
-close.addEventListener('click', function () {
+popup.addEventListener('click', function(event){
+	if (event.target && event.target.tagName == 'STRONG') {
 		popup.style.display = 'none';
-		document.body.style.overflow = 'hidden';
+	} 
 });
 },60000);
 headerBtn.addEventListener('click', function(){
 popupEngineer.style.display = 'block';
 document.body.style.overflow = 'hidden';
-});
-// for (let i = 0; i < popupDialog.length; i++) {
-// 		popupDialog[i].addEventListener('click', function(){
-// 		popup.style.display = 'none';
-// 		popupEngineer.style.display = 'none';
-// 	});
-// }
-	for (let i = 0; i < close.length; i++) {
-		close[i].addEventListener('click', function () {
-			popup.style.display = 'none';
-			popupEngineer.style.display = 'none';
-		});
+popupEngineer.addEventListener('click', function (event) {
+	if (event.target && event.target.tagName == "STRONG" ) {
+		popupEngineer.style.display = 'none';
+	} if (!popupForm.is(event.target)) {
+		popupEngineer.style.display = 'none';
 	}
-
+});
+});
 }
-
 module.exports = form;
