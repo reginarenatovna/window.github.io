714d4dd51327e00a69c09e05b4e49b211129dae6
Regina <regina-saifieva@mail.ru>
2018-11-02 20:49:52 +0300

    modal

--- src-js/index.js
+++ src-js/index.js
@@ -2,7 +2,9 @@ window.addEventListener('DOMContentLoaded',  function(){
 
 	'use strict';
 	let timer = require('./parts/timer.js'),
-			form = require('./parts/form.js');
+			form = require('./parts/form.js'),
+			tabs = require('./parts/tabs.js');
 	timer();
 	form();
+	tabs();
 });
--- src-js/parts/form.js
+++ src-js/parts/form.js
@@ -2,15 +2,22 @@ function form() {
 	let popup = document.querySelector('.popup'),
 			headerBtn = document.querySelector('.header_btn'),
 			popupForm = document.querySelectorAll('.popup_form'),
+			phone = document.querySelectorAll('.phone_link'),
 			popupEngineer = document.querySelector('.popup_engineer');
 setTimeout(function(){
 popup.style.display = 'block';
-popup.addEventListener('click', function(event){
+let close = popup.addEventListener('click', function(event){
 	if (event.target && event.target.tagName == 'STRONG') {
 		popup.style.display = 'none';
 	} 
 });
 },60000);
+for (let i=0; i < phone.length; i++) {
+phone[i].addEventListener('click', function () {
+	popup.style.display = 'block';
+	close();
+});
+}
 headerBtn.addEventListener('click', function(){
 popupEngineer.style.display = 'block';
 document.body.style.overflow = 'hidden';
