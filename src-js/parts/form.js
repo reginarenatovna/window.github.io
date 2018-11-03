a16e66f3f9ac81a82371338e0761360efb1f1d35
Regina <regina-saifieva@mail.ru>
2018-11-03 15:38:02 +0300

    change form

--- src-js/parts/form.js
+++ src-js/parts/form.js
@@ -12,19 +12,13 @@ let close = popup.addEventListener('click', function(event){
 	} 
 });
 },60000);
-for (let i=0; i < phone.length; i++) {
-phone[i].addEventListener('click', function () {
-	popup.style.display = 'block';
-	close();
-});
-}
 headerBtn.addEventListener('click', function(){
 popupEngineer.style.display = 'block';
 document.body.style.overflow = 'hidden';
 popupEngineer.addEventListener('click', function (event) {
-	if (event.target && event.target.tagName == "STRONG" ) {
-		popupEngineer.style.display = 'none';
-	} if (!popupForm.is(event.target)) {
+	let target = event.target;
+	if (target.closest('.popup_engineer') &&  !target.closest(".popup_form")){
+		event.stopPropagation();
 		popupEngineer.style.display = 'none';
 	}
 });
