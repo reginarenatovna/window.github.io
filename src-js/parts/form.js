5385496a6216f500b3dba89af28191da29cc021f
Regina <regina-saifieva@mail.ru>
2018-11-03 15:59:54 +0300

    change a

--- src-js/parts/form.js
+++ src-js/parts/form.js
@@ -2,26 +2,38 @@ function form() {
 	let popup = document.querySelector('.popup'),
 			headerBtn = document.querySelector('.header_btn'),
 			popupForm = document.querySelectorAll('.popup_form'),
-			phone = document.querySelectorAll('.phone_link'),
+			phone = document.querySelectorAll('.phone_link')[0],
 			popupEngineer = document.querySelector('.popup_engineer');
+headerBtn.addEventListener('click', function () {
+	popupEngineer.style.display = 'block';
+	document.body.style.overflow = 'hidden';
+	popupEngineer.addEventListener('click', function (event) {
+		let target = event.target;
+		if (target.closest('.popup_engineer') && !target.closest(".popup_form")) {
+			event.stopPropagation();
+			popupEngineer.style.display = 'none';
+		}
+	});
+});
 setTimeout(function(){
 popup.style.display = 'block';
-let close = popup.addEventListener('click', function(event){
-	if (event.target && event.target.tagName == 'STRONG') {
-		popup.style.display = 'none';
-	} 
-});
+popup.addEventListener('click', function (event) {
+		let target = event.target;
+		if (target.closest('.popup') && !target.closest(".popup_form")) {
+			event.stopPropagation();
+			popup.style.display = 'none';
+		}
+	});
 },60000);
-headerBtn.addEventListener('click', function(){
-popupEngineer.style.display = 'block';
-document.body.style.overflow = 'hidden';
-popupEngineer.addEventListener('click', function (event) {
-	let target = event.target;
-	if (target.closest('.popup_engineer') &&  !target.closest(".popup_form")){
-		event.stopPropagation();
-		popupEngineer.style.display = 'none';
-	}
-});
+phone.addEventListener('click', function(){
+popup.style.display = 'block';
+popup.addEventListener('click', function (event) {
+		let target = event.target;
+		if (target.closest('.popup') && !target.closest(".popup_form")) {
+			event.stopPropagation();
+			popup.style.display = 'none';
+		}
+	});
 });
 }
 module.exports = form;
