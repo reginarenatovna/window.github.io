f6927629bf283e3074ac4a4a1c8867c8ebcf6355
Regina <regina-saifieva@mail.ru>
2018-11-07 06:33:48 +0300

    change

--- src-js/parts/calc.js
+++ src-js/parts/calc.js
@@ -1,25 +1,25 @@
 function calc() {
 	let btn = document.querySelectorAll('.glazing_price_btn'),
-			popupCalc = document.querySelector('.popup_calc'),
-			popupBtn = document.querySelector('.popup_calc_button'),
-			popupCalcProfile = document.querySelector('.popup_calc_profile'),
-			cheakBox = document.querySelectorAll('.checkbox'),
-			popupBtnProfile = document.querySelector('.popup_calc_profile_button');
-	let obj={};
-	for (let i=0; i<btn.length; i++) {
-		btn[i].addEventListener('click', function(){
+		popupCalc = document.querySelector('.popup_calc'),
+		popupBtn = document.querySelector('.popup_calc_button'),
+		popupCalcProfile = document.querySelector('.popup_calc_profile'),
+		cheakBox = document.querySelectorAll('.checkbox'),
+		popupBtnProfile = document.querySelector('.popup_calc_profile_button'),
+		input = document.getElementsByTagName('input');
+	let obj = {};
+	for (let i = 0; i < btn.length; i++) {
+		btn[i].addEventListener('click', function () {
 			popupCalc.style.display = 'block';
 			document.body.style.overflow = 'hidden';
-			popupCalc.addEventListener('click', function(event){
+			popupCalc.addEventListener('click', function (event) {
 				let target = event.target;
-				if ( target.closest(".popup_calc_close") ) {
+				if (target.closest(".popup_calc_close")) {
 					event.stopPropagation();
 					popupCalc.style.display = 'none';
 					obj = {};
-					let inpCalc = document.querySelectorAll('.popup_calc input');
 					console.log(inpCalc);
-					for (let i = 0; i < inpCalc.length; i++) {
-						inpCalc[i].value = '';
+					for (let i = 0; i < input.length; i++) {
+						input[i].value = '';
 					}
 				}
 			});
@@ -45,53 +45,49 @@ function calc() {
 			}
 		});
 	}
-popupBtn.addEventListener('click', function(){
-	popupCalc.style.display = 'none';
-	popupCalcProfile.style.display = 'block';
-	let e = document.getElementById("view_type");
-	let strUser = e.options[e.selectedIndex].text;
-	obj.type = strUser;
-	let inpCalc = document.querySelectorAll('.popup_calc-input');
-	let width = inpCalc[0].value,
-		hight = inpCalc[1].value;
-	obj.width = width;
-	obj.hight = hight;
+	popupBtn.addEventListener('click', function () {
+		popupCalc.style.display = 'none';
+		popupCalcProfile.style.display = 'block';
+		let e = document.getElementById("view_type");
+		let strUser = e.options[e.selectedIndex].text;
+		obj.type = strUser;
+		let inpCalc = document.querySelectorAll('.popup_calc-input');
+		let width = inpCalc[0].value,
+			hight = inpCalc[1].value;
+		obj.width = width;
+		obj.hight = hight;
 
-	let cheakBox = document.querySelectorAll('.checkbox');
-	popupCalcProfile.addEventListener('click', function (event) {
-		let target = event.target;
-		if (target.closest(".popup_calc_profile_close")) {
-			event.stopPropagation();
-			popupCalcProfile.style.display = 'none';
-			obj = {};
-		}
-	});
-});
-popupBtnProfile.addEventListener('click', function(){
-let popupCalcEnd = document.querySelector('.popup_calc_end');
-	popupCalcProfile.style.display = 'none';
-	popupCalcEnd.style.display = 'block';
-	let inputCalcEnd = document.querySelectorAll('.input_end');
-	obj.name=inputCalcEnd[0].value;
-	obj.phone = inputCalcEnd[1].value;
-	popupCalcEnd.addEventListener('click', function (event) {
-		let target = event.target;
-		if (target.closest(".popup_calc_end_close")) {
-			let inputCalcEnd = document.querySelectorAll('.popup_calc_end input');
-			event.stopPropagation();
-			popupCalcEnd.style.display = 'none';
-			obj = {};
-			for (let i = 0; i < inpCalc.length; i++) {
-				inputCalcEnd[i].value = '';
+		let cheakBox = document.querySelectorAll('.checkbox');
+		popupCalcProfile.addEventListener('click', function (event) {
+			let target = event.target;
+			if (target.closest(".popup_calc_profile_close")) {
+				event.stopPropagation();
+				popupCalcProfile.style.display = 'none';
+				obj = {};
 			}
-			}
+		});
 	});
-});
+	popupBtnProfile.addEventListener('click', function () {
+		let popupCalcEnd = document.querySelector('.popup_calc_end');
+		popupCalcProfile.style.display = 'none';
+		popupCalcEnd.style.display = 'block';
+		popupCalcEnd.addEventListener('click', function (event) {
+			let target = event.target;
+			if (target.closest(".popup_calc_end_close")) {
+				event.stopPropagation();
+				popupCalcEnd.style.display = 'none';
+				obj = {};
+				for (let i = 0; i < input.length; i++) {
+					input[i].value = '';
+				}
+			}
+		});
+	});
 
-let balconIcons = document.querySelector('.balcon_icons'),
-	 	typeBigWindow = document.querySelectorAll('.big_window'),
+	let balconIcons = document.querySelector('.balcon_icons'),
+		typeBigWindow = document.querySelectorAll('.big_window'),
 		typeSmallWin = document.querySelectorAll('.tab-window');
-	
+
 	// Tabs Calc
 	function hidetype_window(a) {
 		for (let i = a; i < typeBigWindow.length; i++) {
@@ -116,64 +112,65 @@ let balconIcons = document.querySelector('.balcon_icons'),
 		}
 	}
 
-		balconIcons.addEventListener('click', function (event) {
-			event.preventDefault();
-			let target = event.target;
-			console.log(target);
-			if (target && target.classList.contains('tab-window')) {
-				for (let i = 0; i < typeSmallWin.length; i++) {
-					if (target == typeSmallWin[i]) {
-						hidetype_window(0);
-						showtype_window(i);
-						break;
-					}
+	balconIcons.addEventListener('click', function (event) {
+		event.preventDefault();
+		let target = event.target;
+		console.log(target);
+		if (target && target.classList.contains('tab-window')) {
+			for (let i = 0; i < typeSmallWin.length; i++) {
+				if (target == typeSmallWin[i]) {
+					hidetype_window(0);
+					showtype_window(i);
+					break;
 				}
 			}
-		});
-		let message = {
-			loading: "Загрузка...",
-			success: 'Спасибо! Мы скоро с вами свяжемся!',
-			failure: 'Что-то пошло не так'
-		};
-	let formEnd = document.querySelector('.popup_form-end');
-			function send(elem) {
-				elem.addEventListener('submit', function (e) {
-					e.preventDefault();
-					let statusMessage = document.createElement('div'),
-							input = document.getElementsByTagName('input');
-					elem.appendChild(statusMessage);
-
-					function postData(data) {
-						return new Promise(function (resolve, reject) {
-							let request = new XMLHttpRequest();
-							request.open('POST', 'server.php');
-							request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-							request.onreadystatechange = function () {
-								if (request.readyState < 4) {
-									resolve();
-								} else if (request.readyState === 4) {
-									if (request.status === 200 && request.status < 300) {
-										resolve();
-									} else {
-										reject();
-									}
-								}
-							};
-							request.send(data);
-						});
-					} // end postData
-					function clearInput() {
-						for (let i = 0; i < input.length; i++) {
-							input[i].value = '';
+		}
+	});
+	let message = {
+		loading: "Загрузка...",
+		success: 'Спасибо! Мы скоро с вами свяжемся!',
+		failure: 'Что-то пошло не так'
+	};
+	let formEnd = document.querySelector('.popup_form-contact_end');
+	function send(elem) {
+		elem.addEventListener('submit', function (e) {
+			e.preventDefault();
+			let statusMessage = document.createElement('div'),
+				input = document.getElementsByTagName('input');
+			elem.appendChild(statusMessage);
+		let 	obj = {}; 
+		
+			function postData(data) {
+				return new Promise(function (resolve, reject) {
+					let request = new XMLHttpRequest();
+					request.open('POST', 'server.php');
+					request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+					request.onreadystatechange = function () {
+						if (request.readyState < 4) {
+							resolve();
+						} else if (request.readyState === 4) {
+							if (request.status === 200 && request.status < 300) {
+								resolve();
+							} else {
+								reject();
+							}
 						}
-					}
-					postData(obj)
-						.then(() => statusMessage.innerHTML = message.loading)
-						.then(() => statusMessage.innerHTML = message.success)
-						.catch(() => statusMessage.innerHTML = message.failure)
-						.then(clearInput);
+					};
+					request.send(data);
 				});
+			} // end postData
+			function clearInput() {
+				for (let i = 0; i < input.length; i++) {
+					input[i].value = '';
+				}
 			}
-send(formEnd);
+			postData(obj)
+				.then(() => statusMessage.innerHTML = message.loading)
+				.then(() => statusMessage.innerHTML = message.success)
+				.catch(() => statusMessage.innerHTML = message.failure)
+				.then(clearInput);
+		});
+	}
+	send(formEnd);
 }
 module.exports = calc;
